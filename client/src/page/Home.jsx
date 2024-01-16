4d4027680e2d3c2f06e98f9dc7fff480cbc18f94
Ibrahim <ibrahim@Ibrahims-MacBook-Pro.local>
2024-01-16 18:26:10 -0500

    setting up client side using vite

---
+++ client/src/page/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-5xl p-3">Crypto Legands</h1>
+      <h2 className="text-3xl p-3"> NFT Combat Cardse</h2>
+      {/* <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p> */}
+    </div>
+  );
+};
+
+export default Home;
